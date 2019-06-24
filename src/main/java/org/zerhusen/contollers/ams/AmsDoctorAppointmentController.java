7673bef6df490c32a94bd41a422fe4db018d7616
Bertin E Mendonsa <bertinmendonsa29@gmail.com>
2019-06-24 17:13:13 +0530

    Commit on 24th June 2019

--- src/main/java/org/zerhusen/contollers/ams/AmsDoctorAppointmentController.java
+++ src/main/java/org/zerhusen/contollers/ams/AmsDoctorAppointmentController.java
@@ -2,17 +2,24 @@ package org.zerhusen.contollers.ams;
 
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerhusen.model.ams.AmsAppointments;
@@ -44,16 +51,54 @@ public class AmsDoctorAppointmentController {
     
     @Autowired
     private UserRepository userRepo;
+    
+    @Autowired
+    private AmsAppointmentsRepository appointmentRepo;
 
+
+	
 	@GetMapping("/getmyappoinntments/{date}")
-	public Iterable<AmsAppointments> getAppointments(@PathVariable("date") String date, HttpServletRequest req) throws ParseException{
+	public Iterable<AmsAppointments> getAppointmentDoctorwise(@PathVariable("date") String date,HttpServletRequest req)throws ParseException{		
 		String token = req.getHeader(tokenHeader).substring(7);
-		    	String username = jwtTokenUtil.getUsernameFromToken(token);
+    	String username = jwtTokenUtil.getUsernameFromToken(token);
     	User user = userRepo.findByEmail(username);
-		LocalDate getdate= LocalDate.parse(date);
-		return appointmentrepo.findAll().stream().filter(i->i.getSlot().getDoctor().equals(user)&&i.getDate().equals(getdate)).collect(Collectors.toList());
+		LocalDate entrydate = LocalDate.parse(date);
+		return appointmentRepo.findAll().stream().filter(i->(i.isActive()==true)&& i.getSlot().getDoctor() == user &&(i.getDate().equals(entrydate)) && (i.isRescheduled()==false)).collect(Collectors.toList());
+	}
+	
+	
+	@GetMapping("/getMonthwiseAppointmentDoctorwise/{date}")
+	public Iterable<AmsAppointments> getMonthwiseAppointmentDoctorwise(@PathVariable("date") String date,HttpServletRequest req)throws ParseException{		
+		String token = req.getHeader(tokenHeader).substring(7);
+    	String username = jwtTokenUtil.getUsernameFromToken(token);
+    	User user = userRepo.findByEmail(username);
+		LocalDate entrydatee = LocalDate.parse(date);
+		int month = entrydatee.getMonthValue();
+		List<AmsAppointments> appoint = new ArrayList<AmsAppointments>();
+		Iterable<AmsAppointments> appo= appointmentRepo.findAll().stream().filter(i->(i.isActive()==true)&& i.getSlot().getDoctor() == user && i.getSlot().getDoctor() == user && i.isCompleted()==false && (i.isRescheduled()==false)).collect(Collectors.toList());
+	for (AmsAppointments amsAppointments : appo) {
+		LocalDate obj = amsAppointments.getDate();
+		int mon=obj.getMonthValue();
+		if(mon==month) {
+			appoint.add(amsAppointments);
+		}
+	}
+	return appoint;
+	}
+	
+	@PostMapping("/getBetweenDateAppiontmentDoctorwise")
+	public ResponseEntity<?> getBetweenDateAppiontmentDoctorwise(@RequestBody String data,HttpServletRequest req) throws JSONException{
+		JSONObject ob = new JSONObject(data);
+		String token = req.getHeader(tokenHeader).substring(7);
+    	String username = jwtTokenUtil.getUsernameFromToken(token);
+    	User user = userRepo.findByEmail(username);
+		LocalDate from = LocalDate.parse(ob.getString("fromDate"));
+		LocalDate to = LocalDate.parse(ob.getString("toDate"));
+		List<AmsAppointments> appoin = appointmentRepo.findAll().stream().filter(i->(i.isActive()==true) && i.isCompleted()==false && (i.isRescheduled()==false) && i.getDate().isAfter(from) && i.getDate().isBefore(to) || i.getDate().equals(from) || i.getDate().equals(to)).collect(Collectors.toList());
+        return ResponseEntity.ok(appoin);
 		
 	}
+
 	
 
 }
